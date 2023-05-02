83c3a10d98f11f4a4486e82a76355dc116b8c411
Ok Youngjin <oyj940331@gmail.com>
2023-05-02 16:59:11 +0900

    feat: crate function to check palindrome
    입력받은 문자열이 회문인지 확인하는 함수 작성

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ def input_word():
     word = input("문자열을 입력하시오 : ")
     return word
 
+def check_palindrome(word):
+    if word == word[::-1]:
+        print('회문입니다')
+    else:
+        print('회문이 아닙니다')
+
 if __name__ == '__main__':
     word = input_word()
-    print(word)
+    check_palindrome(word)
